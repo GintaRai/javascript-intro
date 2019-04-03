9a51bb3bf118b82f6367fb3aa7da3be7d3c74f44
GintaRai <48885593+GintaRai@users.noreply.github.com>
2019-04-03 13:37:13 +0300

    functions test update

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+console.log('*************Namu darbas 5.****************');
+
+function isrinktiRaides(raides) {
+    
+}
+console.log( isrinktiRaides( "abcdefg", 2 ) );
+console.log( isrinktiRaides( "abcdefghijkl", 3 ) );
+console.log( isrinktiRaides( "abc", 0 ) );
+console.log( isrinktiRaides( "abc", 4 ) );
+console.log( isrinktiRaides( 1561, 2 ) );
